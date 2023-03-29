7707e8e7d73bd884ce6b09abf2c0223ec7a3bcf1
Finestwork <Younes.Espiritu@outlook.com>
2023-03-29 17:37:55 +0800

    Fix CSS Layout and Display Active Link Properly

--- dark-hack/index.js
+++ dark-hack/index.js
@@ -1,27 +1,76 @@
 const NAV_BAR = document.getElementById('navBar');
-const NAV_LIST = document.getElementById('navList')
+const NAV_LIST = document.getElementById('navList');
 const HERO_HEADER = document.getElementById('heroHeader');
 const HAMBURGER_BTN = document.getElementById('hamburgerBtn');
+const NAV_LINKS = Array.from( document.querySelectorAll('.nav__list-link'));
 const SERVICE_BOXES = document.querySelectorAll('.service-card__box');
+const ACTIVE_LINK_CLASS = 'active';
+const BREAKPOINT = 576;
 
 let currentServiceBG = null;
+let currentActiveLink = document.querySelector('.nav__list-link.active');
 
+// Remove the active state once the breakpoint is reached
+const resetActiveState = ()=>{
+  NAV_LIST.classList.remove('nav--active');
+  Object.assign(NAV_LIST.style, {
+    height: null
+  });
+  Object.assign(document.body.style, {
+    overflowY: null
+  });
+}
+
+//Add padding to the header to make it visible because navbar has a fixed position.
 const addPaddingToHeroHeaderFn = () => {
   const NAV_BAR_HEIGHT = NAV_BAR.getBoundingClientRect().height;
   const HEIGHT_IN_REM = NAV_BAR_HEIGHT / 10;
 
   // If hamburger button is active, do not add padding
-  if(NAV_LIST.classList.contains('nav--active')){
+  if (NAV_LIST.classList.contains('nav--active')) {
     return;
   }
   Object.assign(HERO_HEADER.style, {
     paddingTop: HEIGHT_IN_REM + 'rem'
   });
 }
-const toggleNavbar = ()=>{
+addPaddingToHeroHeaderFn();
+window.addEventListener('resize', ()=>{
+  addPaddingToHeroHeaderFn();
+
+  // When the navbar is active and the window is being resized, remove the active state once the breakpoint is reached
+  if(window.innerWidth >= BREAKPOINT){
+    addPaddingToHeroHeaderFn();
+    resetActiveState();
+  }
+});
+
+// As the user scrolls, the active link should change based on the section currently displayed on the screen.
+window.addEventListener('scroll', ()=>{
+  const sections = document.querySelectorAll('#heroHeader, #services, #works, #contact');
+
+  // Loop through sections and check if they are visible
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const NAV_BAR_HEIGHT = NAV_BAR.getBoundingClientRect().height;
+    if (window.scrollY >= sectionTop - NAV_BAR_HEIGHT) {
+      const ID = section.getAttribute('id');
+      const LINK = NAV_LINKS.filter(link => {
+        return link.href.includes('#'+ID);
+      })[0];
+      console.log(LINK);
+      currentActiveLink.classList.remove(ACTIVE_LINK_CLASS);
+      LINK.classList.add(ACTIVE_LINK_CLASS);
+      currentActiveLink = LINK;
+    }
+  });
+});
+
+// Shows & hide navbar on smaller screen
+HAMBURGER_BTN.addEventListener('click', ()=>{
   NAV_LIST.classList.toggle('nav--active');
-  if(NAV_LIST.classList.contains('nav--active')){
-    Object.assign(document.body.style,{
+  if (NAV_LIST.classList.contains('nav--active')) {
+    Object.assign(document.body.style, {
       overflowY: 'hidden'
     });
     Object.assign(NAV_LIST.style, {
@@ -32,24 +81,29 @@ const toggleNavbar = ()=>{
   Object.assign(NAV_LIST.style, {
     height: 0
   });
-  Object.assign(document.body.style,{
+  Object.assign(document.body.style, {
     overflowY: null
   });
-}
+});
 
-/* Attach Events */
-addPaddingToHeroHeaderFn();
-window.addEventListener('resize', addPaddingToHeroHeaderFn);
-HAMBURGER_BTN.addEventListener('click', toggleNavbar);
+// When navbar link is clicked, reset the active state
+NAV_LINKS.forEach(link => {
+  link.addEventListener('click', ()=>{
+    resetActiveState();
+    link.blur();
+  })
+})
+
+// Handles the hover animation on services section
 SERVICE_BOXES.forEach(service => {
-  const moveBG = (x, y)=>{
+  const moveBG = (x, y) => {
     Object.assign(currentServiceBG.style, {
       left: x + 'px',
       top: y + 'px',
     })
   }
   service.addEventListener('mouseenter', (e) => {
-    if(currentServiceBG === null){
+    if (currentServiceBG === null) {
       currentServiceBG = service.querySelector('.service-card__bg');
     }
     moveBG(e.clientX, e.clientY);
@@ -67,4 +121,11 @@ SERVICE_BOXES.forEach(service => {
     moveBG(LEFT, TOP);
     currentServiceBG = null;
   });
+});
+
+// Handles smooth scrolling
+new SweetScroll({
+  trigger: '.nav__list-link',
+  easing: 'easeOutQuint',
+  offset: NAV_BAR.getBoundingClientRect().height - 80
 });
