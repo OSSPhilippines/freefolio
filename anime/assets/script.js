f60e2e6c52ddc8b8dd64bc4e8c315eccad8f8c5b
Ralph Rosael <ralphrosael27@gmail.com>
2023-07-22 23:37:59 -0500

    fix: fixed indentation to 2 spaces in HTML, CSS, and JS files

--- anime/assets/script.js
+++ anime/assets/script.js
@@ -2,21 +2,21 @@ let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 };
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
 };
 
 const sr = ScrollReveal({
-    distance: '60px',
-    duration: 2500,
-    reset: true,
+  distance: '60px',
+  duration: 2500,
+  reset: true,
 });
 
-sr.reveal('.home-text', {delay:200, origin:'top'});
-sr.reveal('.home-img', {delay:400, origin:'top'});
-sr.reveal('.about, .cta, .resume, .contact, .footer', {delay:200, origin:'top'});
+sr.reveal('.home-text', { delay: 200, origin: 'top' });
+sr.reveal('.home-img', { delay: 400, origin: 'top' });
+sr.reveal('.about, .cta, .resume, .contact, .footer', { delay: 200, origin: 'top' });
